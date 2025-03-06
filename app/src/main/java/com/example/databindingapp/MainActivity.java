552dc81fecb70507739e36e0df821d99ea994a81
Mohit <Mohitbhatia659@gmail.com>
2025-03-06 23:00:49 +0530

    let's commit

---
+++ app/src/main/java/com/example/databindingapp/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.databindingapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+
+import com.example.databindingapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private ActivityMainBinding activityMainBinding;
+    private  MyClickHandler myClickHandler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+
+        Person p1 = new Person("Mohit", "mohit@gmail.com" );
+
+        activityMainBinding = DataBindingUtil.setContentView(
+                this, R.layout.activity_main);
+
+        activityMainBinding.setPerson(p1);
+
+//        Binding the Handler for Click Events
+        myClickHandler = new MyClickHandler(this);
+        activityMainBinding.setClickHandler(myClickHandler);
+
+        // Two-Way Binding
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
